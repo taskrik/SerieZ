e2646e3fe78a0eee05b58fa04a86bd179b81e0de
taskrik <tasos.krikonis@gmail.com>
2020-04-04 20:27:56 +0200

    Modifies navigator and screens

--- src/navigation/homeStack.js
+++ src/navigation/homeStack.js
@@ -1,7 +1,12 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
+
 import * as React from 'react';
 
 import HomePage from '../components/HomePage';
+import SerieDetails from '../components/SerieDetails';
 
 const Stack = createStackNavigator();
 
@@ -9,11 +14,30 @@ function HomeStack() {
   return (
     <Stack.Navigator
       initialRouteName="Home"
-      screenOptions={{ gestureEnabled: false }}>
+      screenOptions={{
+        gestureEnabled: false,
+        headerBackTitle: false,
+        headerStyle: {
+          backgroundColor: '#E50914',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 18,
+        },
+        headerTitleAlign: 'center',
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}>
       <Stack.Screen
         name="Home"
         component={HomePage}
         options={{ title: 'My app', headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="SerieDetails"
+        component={SerieDetails}
+        options={{ title: 'Info' }}
       />
     </Stack.Navigator>
   );
